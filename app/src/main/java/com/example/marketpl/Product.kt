598b9becb545fec83489839125036e6888b3c567
4seokhyeon <tktjrgus@naver.com>
2023-08-24 11:45:08 +0900

    first commit

---
+++ app/src/main/java/com/example/marketpl/Product.kt
@@ -0,0 +1,7 @@
+package com.example.marketpl
+
+data class Product(
+    val imageFileName: Int, val productName: String, val productInfo: String,
+    val sellName: String, val price: Int, val address: String, val likeCount: Int,
+    val chatCount: Int
+)
