2f0095a4cd96161a62cc0602640006bb633512ea
4seokhyeon <tktjrgus@naver.com>
2023-08-31 19:13:36 +0900

    [DOCS] : 📚 좋아요 버튼 스냅바

--- app/src/main/java/com/example/marketpl/DetailActivity.kt
+++ app/src/main/java/com/example/marketpl/DetailActivity.kt
@@ -3,17 +3,22 @@ package com.example.marketpl
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.example.marketpl.anim.slideLeft
 import com.example.marketpl.anim.slideRight
 import com.example.marketpl.databinding.ActivityDetailBinding
 import com.example.marketpl.dataclass.Product
 import com.example.marketpl.datamember.ProductManagerImpl
+import com.google.android.material.snackbar.Snackbar
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
     private lateinit var currentProduct: Product
     private var isLiked = false
+    private lateinit var coordinatorLayout: CoordinatorLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +30,8 @@ class DetailActivity : AppCompatActivity() {
 
         val products = ProductManagerImpl.getInstance().getProducts()  // 싱글톤인 ProductManagerImpl에 대한 리스트를 가져와 products에 저장
         currentProduct = products[itemIndex]  //리스트에 있는 정보를 currentProduct에 저장
+
+        coordinatorLayout = binding.coordinatorLayout
 
         setBindings() //메소드 호
         setLiker()
@@ -52,12 +59,17 @@ class DetailActivity : AppCompatActivity() {
             currentProduct.likeCount += if (isLiked) 1 else -1
             updateLikeButtonState()
             setResultAndFinish()
+
+            val snackbar = if (isLiked) "좋아요를 눌렀습니다" else "좋아요를 취소했습니다."
+            showSnackbar(snackbar)
         }
     }
 
     private fun updateLikeButtonState() { //좋아요 버튼 변경 메소드
         binding.likeBtn.setImageResource(
-            if (isLiked) R.drawable.like_fill
+            if (isLiked) {
+                R.drawable.like_fill
+            }
             else R.drawable.like_btn
         )
     }
@@ -69,6 +81,16 @@ class DetailActivity : AppCompatActivity() {
         }
         setResult(Activity.RESULT_OK, resultIntent) //액티비티가 종료되면서 결과를 intent
 
+    }
+
+    private fun showSnackbar(message: String) {
+        val snackbar = Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_SHORT) // 짧은 지속 시간으로 변경
+        snackbar.show()
+
+        // 3초 후에 스낵바 사라지도록 설정
+        Handler(Looper.getMainLooper()).postDelayed({
+            snackbar.dismiss()
+        }, 3000)
     }
 
     override fun onBackPressed() {
