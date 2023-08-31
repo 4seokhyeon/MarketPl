bae96ef37c100a3d095c35c5691659d44bcf99e4
4seokhyeon <tktjrgus@naver.com>
2023-08-31 14:32:20 +0900

    [DOCS] : 📚 어댑터 수정 및 주석

--- app/src/main/java/com/example/marketpl/ProductAdapter.kt
+++ app/src/main/java/com/example/marketpl/ProductAdapter.kt
@@ -4,106 +4,71 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.marketpl.anim.slideRight
 import com.example.marketpl.databinding.ItemViewBinding
 import com.example.marketpl.dataclass.Product
-import com.example.marketpl.datamember.ProductManagerImpl
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.text.NumberFormat
-import java.util.Locale
 
 class ProductAdapter(private val context: Context, products: List<Product>) :
     RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
+
     private val productList: MutableList<Product> = products.toMutableList()
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): ProductAdapter.ProductViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
         val binding = ItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ProductViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ProductAdapter.ProductViewHolder, position: Int) {
-        Log.d("Debug", "ProductAdapter - Clicked item index: $position")
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
         val currentProduct = productList[position]
         holder.bind(currentProduct)
 
         holder.itemView.setOnClickListener {
-            val intent = Intent(context, DetailActivity::class.java)
-            intent.putExtra("productImage", currentProduct.imageFileName)
-            intent.putExtra("productName", currentProduct.productName)
-            intent.putExtra("productInfo", currentProduct.productInfo)
-            intent.putExtra("userName", currentProduct.sellName)
-            intent.putExtra("productPrice", currentProduct.price)
-            intent.putExtra("userLoc", currentProduct.address)
-            intent.putExtra("itemIndex", position)
-            intent.putExtra("isLiked", currentProduct.isLike) // 추가
-            // 여기에 다른 필요한 정보들도 추가할 수 있습니다.
-            (context as? Activity)?.startActivityForResult(intent, DETAIL_REQUEST_CODE)
-
+            val intent = Intent(context, DetailActivity::class.java).apply {
+                putExtra("productImage", currentProduct.imageFileName)
+                putExtra("productName", currentProduct.productName)
+                putExtra("productInfo", currentProduct.productInfo)
+                putExtra("userName", currentProduct.sellName)
+                putExtra("productPrice", currentProduct.price)
+                putExtra("userLoc", currentProduct.address)
+                putExtra("itemIndex", position)
+                putExtra("isLiked", currentProduct.isLike)
+            }
+            (context as? Activity)?.startActivityForResult(intent, DETAIL_REQUEST_CODE) // 어댑터에서의 정보를 선언해주며 데이터를 데테일 엑티비티로 넘겨줌
         }
     }
 
-    override fun getItemCount(): Int {
-        return productList.size
-    }
+    override fun getItemCount(): Int = productList.size //리스트의 갯수만큼 보여준다
 
-    fun getItem(position: Int): Product {
-        return productList[position]
-    }
-
-    fun getItemIndex(product: Product): Int {
-        return productList.indexOf(product)
-    }
-
-    inner class ProductViewHolder(private val binding: ItemViewBinding) :
+    inner class ProductViewHolder(private val binding: ItemViewBinding) : //item xml에 데이터를 맞게 띄워줌
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(product: Product) {
-            binding.productImage.setImageResource(product.imageFileName)
-            binding.productName.text = product.productName
-            binding.productloc.text = product.address
-            val priceFormat = NumberFormat.getNumberInstance(Locale.getDefault())
-            val formattedPrice = priceFormat.format(product.price)
-            val priceText = "$formattedPrice 원"
-            binding.productPrice.text = priceText
-            binding.comentTx.text = product.chatCount.toString()
-            binding.favariteTx.text = product.likeCount.toString()
+            with(binding) {
+                productImage.setImageResource(product.imageFileName)
+                productName.text = product.productName
+                productloc.text = product.address
+                productPrice.text = "${product.price} 원"
+                comentTx.text = product.chatCount.toString()
+                favariteTx.text = product.likeCount.toString()
 
-            if (product.isLike) {
-                binding.likeBtn.setImageResource(R.drawable.like_fill)
+                likeBtn.setImageResource(
+                    if (product.isLike) R.drawable.like_fill
+                    else R.drawable.like_btn
+                )
 
-            } else {
-                binding.likeBtn.setImageResource(R.drawable.like_btn)
-            }
-
-            binding.likeBtn.setOnClickListener {
-                if (!product.isLike) { // 좋아요 상태가 false일 때만 처리
-                    product.isLike = true
-                    product.likeCount++
-                    binding.likeBtn.setImageResource(R.drawable.like_fill)
-
-                    // 여기서 업데이트된 정보를 다시 메인 엑티비티로 전달하거나 어떤 작업을 수행할 수 있습니다.
-                } else {
-                    product.isLike = false
-                    product.likeCount--
-                    binding.likeBtn.setImageResource(R.drawable.like_btn)
-
-                    // 여기서 업데이트된 정보를 다시 메인 엑티비티로 전달하거나 어떤 작업을 수행할 수 있습니다.
+                likeBtn.setOnClickListener {
+                    product.isLike = !product.isLike // 변수 뒤집기
+                    if (product.isLike) product.likeCount++ //isLike의 유뮤를 확인하여 count를 증,감소
+                    else product.likeCount--
+                    notifyDataSetChanged()
                 }
             }
-
         }
 
         init {
-            binding.root.setOnLongClickListener {
+            binding.root.setOnLongClickListener {//롱클릭 이벤트 리스트 삭제 기능
                 val position = adapterPosition
                 if (position != RecyclerView.NO_POSITION) {
                     val currentProduct = productList[position]
@@ -113,7 +78,7 @@ class ProductAdapter(private val context: Context, products: List<Product>) :
             }
         }
 
-        private fun showDeleteConfirmationDialog(product: Product, position: Int) {
+        private fun showDeleteConfirmationDialog(product: Product, position: Int) { // 리스트 삭제 메소드
             val builder = AlertDialog.Builder(binding.root.context)
             builder.setTitle("삭제하기")
                 .setMessage("${product.productName}을(를) 삭제하시겠습니까?")
@@ -127,6 +92,6 @@ class ProductAdapter(private val context: Context, products: List<Product>) :
     }
 
     companion object {
-        const val DETAIL_REQUEST_CODE = 0 // 원하는 값으로 설정
+        const val DETAIL_REQUEST_CODE = 0
     }
 }
